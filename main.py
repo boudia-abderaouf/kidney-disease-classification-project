5df56e102aaae4f334cd64fc866649e939ffeeac
boudia-abderaouf <abderaouf.boudia@g.enp.edu.dz>
2024-11-26 22:57:34 +0100

    data ingestion added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import os
-from box.exceptions import BoxValueError
-import yaml 
 from cnnClassifier import logger
-import json
-import joblib
-from ensure import ensure_annotations  
-from box import ConfigBox
-from pathlib import Path 
-from typing import Any
-import base64
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
